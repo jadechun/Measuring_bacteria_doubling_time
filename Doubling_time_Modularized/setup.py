7c15c6971167c4008b335e02097300fbd0fb6717
jadechun <CHUN_YEYU@YAHOO.COM.SG>
2022-12-13 00:35:15 +0800

    Removed obsolete files
    Original source code available at https://github.com/huoww07/calulate_bacteria_doubling_time

--- Doubling_time_Modularized/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(name='Doubling_time_Modularized',
-      version='0.1',
-      description='Doubling time calculation',
-      packages=['Doubling_time_Modularized'],
-      zip_safe=False)
